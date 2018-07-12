20e391c9382111c6e96ab6bc0d6e6b903111ae0b
Samudra Harapan Bekti <sabekti@cisco.com>
2018-07-11 23:11:37 -0700

    Add GET endpoint for dongle status.

--- main.go
+++ main.go
@@ -17,6 +17,29 @@ type message struct {
 	Content   string `json:"content,omitempty"`
 }
 
+func getStatus(w http.ResponseWriter, r *http.Request) {
+	binary, lookErr := exec.LookPath("asterisk")
+	if lookErr != nil {
+		panic(lookErr)
+	}
+
+	cmd := exec.Command(binary, "-rx", "dongle show devices")
+
+	cmd.Env = os.Environ()
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", out.String())
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte(out.String()))
+}
+
 func sendMessage(w http.ResponseWriter, r *http.Request) {
 	var message message
 	_ = json.NewDecoder(r.Body).Decode(&message)
@@ -48,6 +71,7 @@ func sendMessage(w http.ResponseWriter, r *http.Request) {
 func main() {
 	port := os.Args[1]
 	router := mux.NewRouter()
+	router.HandleFunc("/", getStatus).Methods("GET")
 	router.HandleFunc("/", sendMessage).Methods("POST")
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
